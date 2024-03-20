9f60a848526a46f482b36128870156a5157fb522
jerinzech <jerinzjames@gmail.com>
2024-03-21 02:50:21 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Kitsungi from "./kintsugi_hs/Kitsungi";
+
+function App() {
+  return (
+    <>
+      <Kitsungi />
+    </>
+  );
+}
+
+export default App;
---
+++ src/kintsugi_hs/Kitsungi.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Kitsungi.css";
+
+export default function Kitsungi() {
+  return (
+    <div className="body">
+      <img src="src/kintsugi_hs/crackwithbg.svg" alt="tile" className="crack" />
+      <img src="src/kintsugi_hs/pngwing 2.svg" alt="tile" className="PTile" />
+    </div>
+  );
+}
